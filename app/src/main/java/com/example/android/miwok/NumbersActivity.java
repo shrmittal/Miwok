a71d689be86c849f95cb0621585c4b18d0185cb8
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-12 18:02:03 +0530

    create a dynamic textview and displayed elements using while loop in the layout

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,8 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -23,10 +25,13 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
-        Log.v("NumbersActivity","Word at 0 : "+words.get(0));
-        Log.v("NumbersActivity","Word at 1 : "+words.get(1));
-        Log.v("NumbersActivity","Word at 2 : "+words.get(2));
-        Log.v("NumbersActivity","Word at 3 : "+words.get(3));
-        Log.v("NumbersActivity","Word at 4 : "+words.get(4));
+        int index=0;
+        while(index<words.size()) {
+            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+            TextView wordView=new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index=index+1;
+        }
     }
 }
