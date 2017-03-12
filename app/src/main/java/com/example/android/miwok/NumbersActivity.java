2629bd36fda4108310dfb7ce86705724bfa78176
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-12 19:07:15 +0530

    use list view and arrayAdapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,10 +3,13 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -26,8 +29,8 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("nine");
         words.add("ten");
 
-        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        int index = 0;
+       /* LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+        int index = 0;*/
        /* while(index<words.size()) {
 
             TextView wordView=new TextView(this);
@@ -35,10 +38,14 @@ public class NumbersActivity extends AppCompatActivity {
             rootView.addView(wordView);
             index=index+1;
         }*/
-        for (index = 0; index < words.size(); index++) {
+       /* for (index = 0; index < words.size(); index++) {
             TextView wordView = new TextView(this);
             wordView.setText(words.get(index));
             rootView.addView(wordView);
-        }
+        }*/
+
+        ArrayAdapter<String> itemsAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,words);
+        ListView listView=(ListView)findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
