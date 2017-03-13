85ff565b9268890a94e59ab59335c98894fa373d
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-13 16:33:31 +0530

    coding task lesson 2 complete. shows all lists properly

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,25 +2,19 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+        setContentView(R.layout.word_list);
         ArrayList<Word> words = new ArrayList<Word>();
 
-       // words.add("one");
+
         words.add(new Word("one","lutti"));
         words.add(new Word("two","otiiko"));
         words.add(new Word("three","tolookosu"));
