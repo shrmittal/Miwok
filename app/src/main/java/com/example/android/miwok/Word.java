665badacb90a4ab0aa58ff96743ebeca8d13b1df
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-14 21:09:42 +0530

    added audio files correctly

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -4,31 +4,41 @@ package com.example.android.miwok;
  * Created by SHREYA on 3/12/2017.
  */
 public class Word {
-     private String mDefaultTranslation;
+    private String mDefaultTranslation;
     private String mMiwokTranslation;
-    private int mresource=NO_IMAGE_PROVIDED;
-    private static final int NO_IMAGE_PROVIDED=-1;
-public Word(String defaultTranslation,String miwokTranslation)
-{
-    mDefaultTranslation=defaultTranslation;
-    mMiwokTranslation=miwokTranslation;
-}
-    public Word(String defaultTransaction,String miwokTranslation,int resource)
-    {
-        mDefaultTranslation=defaultTransaction;
-        mMiwokTranslation=miwokTranslation;
-        mresource=resource;
+    private int mresource = NO_IMAGE_PROVIDED;
+    private int mAudio;
+    private static final int NO_IMAGE_PROVIDED = -1;
+
+    public Word(String defaultTranslation, String miwokTranslation,int audio) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mAudio=audio;
     }
 
-    public String getDefaultTranslation(){
+    public Word(String defaultTransaction, String miwokTranslation, int resource,int audio) {
+        mDefaultTranslation = defaultTransaction;
+        mMiwokTranslation = miwokTranslation;
+        mresource = resource;
+        mAudio=audio;
+    }
+
+    public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
-    public String getMiwokTranslation(){
+
+    public String getMiwokTranslation() {
         return mMiwokTranslation;
     }
-    public int getResource(){return mresource;}
 
-    public boolean hasImage(){
-        return mresource!=NO_IMAGE_PROVIDED;
+    public int getResource() {
+        return mresource;
+    }
+    public int getAudio(){
+        return mAudio;
+    }
+
+    public boolean hasImage() {
+        return mresource != NO_IMAGE_PROVIDED;
     }
 }
