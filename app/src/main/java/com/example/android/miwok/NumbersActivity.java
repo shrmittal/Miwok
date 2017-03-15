10a77075a1431ce1edbd70ea59e9f778cfe52a18
SHREYA MITTAL <mittalshreya93@gmail.com>
2017-03-15 15:37:08 +0530

    fragments created

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,136 +1,16 @@
 package com.example.android.miwok;
 
-import android.app.ActionBar;
-
-import android.content.Context;
-import android.content.Intent;
-import android.media.AudioManager;
-import android.media.MediaPlayer;
-import android.provider.MediaStore;
-import android.support.v4.app.NavUtils;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    private MediaPlayer mMediaPlayer;
-    private AudioManager mAudioManager;
-
-    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
-            new AudioManager.OnAudioFocusChangeListener() {
-
-                public void onAudioFocusChange(int focusChange) {
-                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT || focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
-                        mMediaPlayer.pause();
-                        mMediaPlayer.seekTo(0);
-
-                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
-                        mMediaPlayer.start();
-
-                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
-                        releaseMediaPlayer();
-                    }
-                }
-            };
-
-
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            releaseMediaPlayer();
-        }
-    };
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.word_list);
-
-
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-
-
-        final ArrayList<Word> words = new ArrayList<Word>();
-
-
-        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyissa", R.drawable.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo'e", R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na'aacha", R.drawable.number_ten, R.raw.number_ten));
-
-       /* LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-        int index = 0;*/
-       /* while(index<words.size()) {
-
-            TextView wordView=new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-            index=index+1;
-        }*/
-       /* for (index = 0; index < words.size(); index++) {
-            TextView wordView = new TextView(this);
-            wordView.setText(words.get(index));
-            rootView.addView(wordView);
-        }*/
-
-        WordAdapter itemsAdapter = new WordAdapter(this, words, R.color.category_numbers);
-        final ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(itemsAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                                            @Override
-                                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                                                //Toast.makeText(NumbersActivity.this, "List item clicked", Toast.LENGTH_SHORT).show();
-                                                Word word = words.get(i);
-
-
-                                                releaseMediaPlayer();
-
-                                                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
-                                                        AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                                                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-
-
-                                                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudio());
-                                                    mMediaPlayer.start();
-                                                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
-                                                }
-        /*GridView gridView=(GridView)findViewById(R.id.list);
-        gridView.setAdapter(itemsAdapter);*/
-                                            }
-                                        }
-        );
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        releaseMediaPlayer();
-    }
-
-    private void releaseMediaPlayer() {
-        if (mMediaPlayer != null) {
-            mMediaPlayer.release();
-            ;
-            mMediaPlayer = null;
-        }
-        mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
-    }
-
-
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(savedInstanceState);
+   setContentView(R.layout.activity_category);
+   getSupportFragmentManager().beginTransaction()
+           .replace(R.id.container, new NumbersFragment())
+           .commit();
+   }
 }
